77d2ec9b3c0ae3d3a53eec81692d0a33aea74b4c
mahendra pankaj <pankajmahendra1006@gmail.com>
2023-03-22 11:14:33 +0530

    Practice

---
+++ perl practice/dowhiledemo.pl
@@ -0,0 +1,7 @@
+do{
+print "Enter name";
+$nm=<>;
+print "Wants to enter more(y/n)?\n";
+$ans=<>;   
+chomp($ans);
+}while($ans eq  "y");
---
+++ perl practice/whiledemo.pl
@@ -0,0 +1,6 @@
+print "enter number";
+$num=<>;
+while($num > 0){
+print "enter number";
+$num=<>;
+}
